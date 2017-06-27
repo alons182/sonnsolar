db6639dbcc34e7474a437422483fe3eea3aeee2f
alonsoavotz <alonso@avotz.com>
2017-06-27 17:36:36 -0600

    update link mapa

--- wp-content/themes/sonnsolar/template-parts/contact-map.php
+++ wp-content/themes/sonnsolar/template-parts/contact-map.php
@@ -1,3 +1,3 @@
 <div class="content__contact__map">
-        <iframe src="https://www.google.com/maps/d/embed?mid=zVBPAwVMF2RM.k7mQM29TFJic" width="640" height="480"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15717.190789715809!2d-84.2181024!3d9.9922496!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8fa0f9ab85fca597%3A0x82cfa137555e9c49!2sSONNSOLAR!5e0!3m2!1ses!2scr!4v1498606563591" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
  </div>
